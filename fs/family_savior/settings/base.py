cfbe22aca5e65212f88c4c31770a0228598df94c
abuubaida01 <abuubaida901@gmail.com>
2023-01-18 11:22:50 +0500

    ready for production

--- fs/family_savior/settings/base.py
+++ fs/family_savior/settings/base.py
@@ -6,8 +6,7 @@ import smtplib, ssl
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
-secret_key = 'django-insecure-9x5e3shen*+a66vk360$0ncpk^4+!o4(mps)e_6tmih(smob^e'
+SECRET_KEY = 'django-insecure-9x5e3shen*+a66vk360$0ncpk^4+!o4(mps)e_6tmih(smob^e'
 
 DEBUG = True
 
--- fs/family_savior/settings/dev.py
+++ fs/family_savior/settings/dev.py
@@ -1,8 +1,7 @@
 from .base import *
 import os
 
-with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = 'django-insecure-9x5e3shen*+a66vk360$0ncpk^4+!o4(mps)e_6tmih(smob^e'
 
 DEBUG = True
 
