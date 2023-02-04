ba46a0765c0834c88f0f1e627ad31c6a807bd346
abuubaida01 <abuubaida901@gmail.com>
2023-02-04 20:50:58 +0500

    created simple test on profile

--- fs/profiles/tests.py
+++ fs/profiles/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
+from user.models import User
+from .models import Profile
 
 # Create your tests here.
 class ProfileTestCast(TestCase):
+  def setUp(self):
+    User.objects.create(username='test', password='test', email='test@gamil.com', full_name='test', religion='Muslim')
+
   def test_failure(self):
-    self.assertEqual(False)
+    qs=User.objects.all()
+    self.assertEqual(qs.exists(), True)
