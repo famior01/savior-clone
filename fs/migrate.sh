4d8c0d348ccfb9cd3694945cbcc03f76b76e6fb5
abuubaida01 <abuubaida901@gmail.com>
2023-02-07 15:49:08 +0500

    added two lines rollout status and restart

--- fs/migrate.sh
+++ fs/migrate.sh
@@ -5,18 +5,18 @@ SUPERUSER_RELIGION="Islam"
 SUPERUSER_USERNAME="abuubaida01"
 cd /app/
 
-/opt/venv/bin/python3 manage.py makemigrations
-/opt/venv/bin/python3 manage.py migrate 
+/opt/venv/bin/python manage.py makemigrations
+/opt/venv/bin/python manage.py migrate 
 
-/opt/venv/bin/python3 manage.py makemigrations user 
-/opt/venv/bin/python3 manage.py migrate user
+/opt/venv/bin/python manage.py makemigrations user 
+/opt/venv/bin/python manage.py migrate user
 
-/opt/venv/bin/python3 manage.py makemigrations profiles 
-/opt/venv/bin/python3 manage.py migrate profiles
+/opt/venv/bin/python manage.py makemigrations profiles 
+/opt/venv/bin/python manage.py migrate profiles
 
-/opt/venv/bin/python3 manage.py migrate --run-syncdb
+/opt/venv/bin/python manage.py migrate --run-syncdb
 
-/opt/venv/bin/python3 manage.py createsuperuser --email $SUPERUSER_EMAIL  --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME  --religion $SUPERUSER_RELIGION --noinput || true
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL  --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME  --religion $SUPERUSER_RELIGION --noinput || true
 
 # celery -A family_savior worker -l INFO -P eventlet
 
