8ff2d1603963d0e99b838cb98eb61469aeba3acc
dadajan01 <abuubaida901@gmail.com>
2023-02-09 18:16:01 +0500

    nothing

--- fs/entrypoint.sh
+++ fs/entrypoint.sh
@@ -2,4 +2,4 @@
 APP_PORT=${PORT}
 # HOST=${DJANGO_ALLOWED_HOSTS:-"104.248.98.3"}
 cd /app/
-/opt/venv/bin/gunicorn --workers=1  --worker-class=gthread family_savior.wsgi:application --bind "104.248.98.3:${APP_PORT}"
+/opt/venv/bin/gunicorn --workers=1  --worker-class=gthread family_savior.wsgi:application --bind "104.248.98.3:${APP_PORT}" 
