96b60d55d380159dcac8492a5a96468946102654
abuubaida01 <abuubaida901@gmail.com>
2023-02-09 08:23:29 +0500

    added host :)

--- fs/entrypoint.sh
+++ fs/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-APP_PORT=${PORT:-8000}
+APP_PORT=${PORT}
+HOST=${DJANGO_ALLOWED_HOSTS:-"104.248.98.131"}
 cd /app/
-/opt/venv/bin/gunicorn --workers=1  --worker-class=gthread family_savior.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+/opt/venv/bin/gunicorn --workers=1  --worker-class=gthread family_savior.wsgi:application --bind "${HOST}:${APP_PORT}"
