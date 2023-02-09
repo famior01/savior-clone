142ddae1412efc8efc6c9769ad394c6eed4326c2
dadajan01 <abuubaida901@gmail.com>
2023-02-09 22:58:19 +0500

    again try

--- fs/migrate.sh
+++ fs/migrate.sh
@@ -7,15 +7,14 @@ cd /app/
 
 /opt/venv/bin/python manage.py makemigrations
 /opt/venv/bin/python manage.py migrate 
+
+/opt/venv/bin/python manage.py makemigrations user 
+/opt/venv/bin/python manage.py migrate user
+
+/opt/venv/bin/python manage.py makemigrations profiles 
+/opt/venv/bin/python manage.py migrate profiles
+
 /opt/venv/bin/python manage.py migrate --run-syncdb
-
-
-# /opt/venv/bin/python manage.py makemigrations user 
-# /opt/venv/bin/python manage.py migrate user
-
-# /opt/venv/bin/python manage.py makemigrations profiles 
-# /opt/venv/bin/python manage.py migrate profiles
-
 
 /opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL  --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME  --religion $SUPERUSER_RELIGION --noinput || true
 
