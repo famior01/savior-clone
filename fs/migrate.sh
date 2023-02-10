e1ae8b17d3051ac85c625011a40dcf96c4fca626
dadajan01 <abuubaida901@gmail.com>
2023-02-10 16:25:52 +0500

    changed structure

--- fs/migrate.sh
+++ fs/migrate.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
-SUPERUSER_EMAIL="abuubaida901@gmail.com"
-SUPERUSER_FULLNAME="Abu-Ubaida"
-SUPERUSER_RELIGION="Islam"
-SUPERUSER_USERNAME="abuubaida01"
 cd /app/
 
-/opt/venv/bin/python manage.py makemigrations
-/opt/venv/bin/python manage.py migrate 
+pip3 install -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate 
+python3 manage.py migrate --run-syncdb
 
-/opt/venv/bin/python manage.py makemigrations user 
-/opt/venv/bin/python manage.py migrate user
-
-/opt/venv/bin/python manage.py makemigrations profiles 
-/opt/venv/bin/python manage.py migrate profiles
-
-/opt/venv/bin/python manage.py migrate --run-syncdb
-
-
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL  --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME  --religion $SUPERUSER_RELIGION --noinput || true
-
-/opt/venv/bin/celery -A family_savior worker -l INFO 
+celery -A family_savior worker -l INFO 
 
---
+++ fs/superuser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /app/
+
+SUPERUSER_EMAIL="abuubaida901@gmail.com"
+SUPERUSER_FULLNAME="Abu-Ubaida"
+SUPERUSER_RELIGION="Islam"
+SUPERUSER_USERNAME="abuubaida01"
+
+python3 manage.py createsuperuser --email $SUPERUSER_EMAIL  --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME  --religion $SUPERUSER_RELIGION --noinput || true
