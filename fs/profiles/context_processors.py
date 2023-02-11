739fd4fb1bb4883d23fb02f238805dfac1e2d8da
dadajan01 <abuubaida901@gmail.com>
2023-02-11 18:29:03 +0500

    try again

--- fs/profiles/context_processors.py
+++ fs/profiles/context_processors.py
@@ -1,4 +1,7 @@
 from .models import Profile
+from decouple import config
+
+Production=config('USE_PRODUCTION', default=False, cast=bool)
 
 def profile_picture(request):
   '''
@@ -8,6 +11,6 @@ def profile_picture(request):
       user = request.user # for notifications
       profiles = Profile.objects.all()
       my_profile = Profile.objects.get(user=request.user) # for profile picture
-      return {'profile_picture': my_profile.picture.url, 'user': user, 'profiles': profiles, 'my_profile': my_profile}
+      return {'profile_picture': my_profile.picture.url, 'user': user, 'profiles': profiles, 'my_profile': my_profile, 'Production': Production}
   return {}
 
