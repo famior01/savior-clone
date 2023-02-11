349deef74928cacd599e4438bb3337583e2bf320
dadajan01 <abuubaida901@gmail.com>
2023-02-11 09:35:28 +0500

    INSHALLAH THIS TIME

--- fs/migrate.sh
+++ fs/migrate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd /app/
 
-/opt/venv/bin/python manage.py makemigrations
-/opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py migrate --run-syncdb --noinput
+# /opt/venv/bin/pip install -r requirements.txt
+/opt/venv/bin/python manage.py makemigrations user
+/opt/venv/bin/python manage.py migrate 
+/opt/venv/bin/python manage.py migrate --run-syncdb 
 
-/opt/venv/bin/celery -A family_savior worker -l INFO 
+# /opt/venv/bin/celery -A family_savior worker -l INFO 
 
 
