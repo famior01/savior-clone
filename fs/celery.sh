a27b31371abafe69ce647a7cef7be5ccebb5e1fa
dadajan01 <abuubaida901@gmail.com>
2023-02-11 23:45:25 +0500

    let's try again after changing alot

--- fs/celery.sh
+++ fs/celery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /app/
-/opt/venv/bin/celery -A family_savior worker -l INFO 
+/opt/venv/bin/celery -A family_savior worker --without-heartbeat --without-gossip --without-mingle -l INFO  -P eventlet -c 1000
