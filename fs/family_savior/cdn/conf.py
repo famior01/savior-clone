d5a0a0e155d9bc0d5bc33309cafd16e9794fa2f6
dadajan01 <abuubaida901@gmail.com>
2023-02-11 15:54:41 +0500

    LET'S TRI AGAIN

--- fs/family_savior/cdn/conf.py
+++ fs/family_savior/cdn/conf.py
@@ -1,14 +1,24 @@
 import os 
 from decouple import config
 
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME ="savior-staticfiles"
+AWS_ACCESS_KEY_ID=config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME="savior-staticfiles"
 AWS_S3_ENDPOINT_URL="https://sgp1.digitaloceanspaces.com"
-AWS_SE_OBJECT_PARAMETERS ={
+
+AWS_SE_OBJECT_PARAMETERS={
   "CacheControl":"max-age=86400",
-  "ACL":"public_read",
+  # "ACL":"public_read",
 }
 AWS_LOCATION="https://savior-staticfiles.sgp1.digitaloceanspaces.com"
+
 DEFAULT_FILE_STORAGE="family_savior.cdn.backends.MediaRootS3BotoStorage"
 STATICFILES_STORAGE="family_savior.cdn.backends.StaticRootS3BotoStorage"
+
+
+# # Use AWS_S3_ENDPOINT_URL here if you haven't enabled the CDN and got a custom domain. 
+# STATIC_URL = '{}/{}/'.format(AWS_LOCATION, 'static')
+# STATIC_ROOT = 'static/'
+
+# MEDIA_URL = '{}/{}/'.format(AWS_LOCATION, 'media')
+# MEDIA_ROOT = 'media/'
--- fs/superuser.sh
+++ fs/superuser.sh
@@ -4,5 +4,6 @@ SUPERUSER_EMAIL="abuubaida901@gmail.com"
 SUPERUSER_FULLNAME="Abu-Ubaida"
 SUPERUSER_RELIGION="Islam"
 SUPERUSER_USERNAME="abuubaida01"
+SUPERUSER_PASSWORD="abuubaida"
 
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL  --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME  --religion $SUPERUSER_RELIGION --noinput || true
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL  --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME  --religion $SUPERUSER_RELIGION --password $SUPERUSER_PASSWORD --noinput || true
