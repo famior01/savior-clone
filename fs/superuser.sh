74e8cdf6e7a453b5b3d600b9d3e5c27bb44d1add
dadajan01 <abuubaida901@gmail.com>
2023-02-12 14:19:32 +0500

    succeeded

--- fs/superuser.sh
+++ fs/superuser.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
+SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
 cd /app/
-SUPERUSER_EMAIL="abuubaida901@gmail.com"
-SUPERUSER_FULLNAME="Abu-Ubaida"
-SUPERUSER_RELIGION="Islam"
-SUPERUSER_USERNAME="abuubaida01"
-SUPERUSER_PASSWORD="Fs1|@Fr2|7*"
-
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL  --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME  --religion $SUPERUSER_RELIGION --password $SUPERUSER_PASSWORD --noinput || true
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
