fe3cb603c4af8370a79664520be016f005b73fdb
dadajan01 <abuubaida901@gmail.com>
2023-02-13 11:47:50 +0500

    nothing

--- fs/entrypoint.sh
+++ fs/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APP_PORT=${PORT:-8000}
 cd /app/
-/opt/venv/bin/gunicorn --workers=2  --worker-class=gthread family_savior.wsgi:application --bind "0.0.0.0:${APP_PORT}" --timeout 600
+/opt/venv/bin/gunicorn --workers=2  --worker-class=gthread family_savior.wsgi:application --bind "0.0.0.0:${APP_PORT}" --timeout 600 
