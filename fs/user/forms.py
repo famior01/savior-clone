4a4d6200fc09a2a7ac66eb03d6b917786553c569
dadajan01 <abuubaida901@gmail.com>
2023-02-14 08:56:35 +0500

    let's see

--- fs/user/forms.py
+++ fs/user/forms.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import User
+from django.contrib.auth import forms as auth_forms
+from django import forms
+
+
 
 
 class UserCreationForm(UserCreationForm):
@@ -10,8 +14,30 @@ class UserCreationForm(UserCreationForm):
         fields = ('email',)
 
 
-class UserChangeForm(UserChangeForm):
+
+class UserChangeForm(auth_forms.UserChangeForm):
+    password = auth_forms.ReadOnlyPasswordHashField(label="Password")
     class Meta:
         model = User
-        fields = ['full_name','username']
-    
+        fields = ['full_name','username'] 
+        
+
+
+# class UserChangeForm(forms.ModelForm):
+#     password = auth_forms.ReadOnlyPasswordHashField(label="Password",
+#         help_text="Raw passwords are not stored, so there is no way to see "
+#                 "this user's password, but you can change the password "
+#                 "using <a  href=\"account/account_change_password/\" >this form</a>.")
+
+#     class Meta:
+#         model = User
+#         fields = '__all__'
+
+#     def __init__(self, *args, **kwargs):
+#         super(UserChangeForm, self).__init__(*args, **kwargs)
+#         f = self.fields.get('user_permissions', None)
+#         if f is not None:
+#             f.queryset = f.queryset.select_related('content_type')
+
+#     def clean_password(self):
+#         return self.initial["password"]
