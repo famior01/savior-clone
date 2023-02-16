f67ded1aa9e62ffcc9c8f3c693a5e6c55a24bfe2
dadajan01 <abuubaida901@gmail.com>
2023-02-16 12:19:31 +0500

    changed user, and other stuff

--- fs/superuser.sh
+++ fs/superuser.sh
@@ -5,7 +5,8 @@ SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-"abuubaida01"}
 SUPERUSER_FULLNAME=${DJANGO_SUPERUSER_FULLNAME:-"Abu-Ubaida"}
 SUPERUSER_RELIGION=${DJANGO_SUPERUSER_RELIGION:-"Islam"}
 SUPERUSER_GENDER=${DJANGO_SUPERUSER_GENDER:-"Male"}
+SUPERUSER_DOB=${DJANGO_SUPERUSER_DOB:-"2001-09-21"}
 
 cd /app/
 
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME --religion $SUPERUSER_RELIGION --gender $SUPERUSER_GENDER --noinput  || true
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --username $SUPERUSER_USERNAME --full_name $SUPERUSER_FULLNAME --religion $SUPERUSER_RELIGION --gender $SUPERUSER_GENDER --date_of_birth $SUPERUSER_DOB --noinput  || true
