9cf31bd177eef6a42291fd83014632e142e15e4c
dadajan01 <abuubaida901@gmail.com>
2023-02-22 07:34:42 +0500

    let's see

--- fs/family_savior/cdn/conf.py
+++ fs/family_savior/cdn/conf.py
@@ -3,13 +3,14 @@ from decouple import config
 
 AWS_ACCESS_KEY_ID=config("AWS_ACCESS_KEY_ID", cast=str)
 AWS_SECRET_ACCESS_KEY=config("AWS_SECRET_ACCESS_KEY", cast=str)
-AWS_STORAGE_BUCKET_NAME="savior-staticfiles"
+AWS_STORAGE_BUCKET_NAME="savior-space"
 AWS_S3_ENDPOINT_URL="https://sgp1.digitaloceanspaces.com"
+
 AWS_SE_OBJECT_PARAMETERS={
   "CacheControl":"max-age=86400",
   "ACL":"public_read", 
 }
 
-AWS_LOCATION="https://savior-staticfiles.sgp1.digitaloceanspaces.com"
+AWS_LOCATION="https://savior-space.sgp1.digitaloceanspaces.com"
 DEFAULT_FILE_STORAGE="family_savior.cdn.backends.MediaRootS3BotoStorage"
 STATICFILES_STORAGE="family_savior.cdn.backends.StaticRootS3BotoStorage"
